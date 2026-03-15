e88eb02736561d81523ae8417af2ef005f81c88d
kei1845 <elsie23418@gmail.com>
2026-03-15 18:57:17 +0900

    top page完成

--- gym-portfolio/footer.php
+++ gym-portfolio/footer.php
@@ -1,58 +1,7 @@
 <footer class="footer">
-    <div class="footer__inner inner">
-      <div class="footer__header">
-        <div class="footer__logo">
-          <a href="<?php echo esc_url( home_url('/') ); ?>">
-            <img src="<?php echo esc_url( get_theme_file_uri('/assets/images/footer-codeups.png') ); ?>" alt="CodeUps">
-          </a>
-        </div>
-        <div class="footer__icons">
-          <a href="#" class="footer__icon hover-opacity">
-            <img src="<?php echo esc_url( get_theme_file_uri('/assets/images/footer-facebook.png') ); ?>" alt="CodeUpsのFacebook">
-          </a>
-          <a href="#" class="footer__icon hover-opacity">
-            <img src="<?php echo esc_url( get_theme_file_uri('/assets/images/footer-instagram.png') ); ?>" alt="CodeUpsのInstagram">
-          </a>
-        </div>
-      </div>
-      <div class="footer__body">
-        <div class="footer__wrapper">
-          <div class="footer__nav-column">
-            <a href="<?php echo esc_url( get_post_type_archive_link('campaign') ); ?>" class="footer__text hover-opacity starfish">キャンペーン</a>
-            <a href="<?php echo esc_url( get_post_type_archive_link('campaign') ); ?>" class="footer__text hover-opacity">ライセンス取得</a>
-            <a href="<?php echo esc_url( get_post_type_archive_link('campaign') ); ?>" class="footer__text hover-opacity">貸切体験ダイビング</a>
-            <a href="<?php echo esc_url( get_post_type_archive_link('campaign') ); ?>" class="footer__text hover-opacity">ナイトダイビング</a>
-            <a href="<?php echo esc_url(home_url("/about-us")) ?>" class="footer__text hover-opacity starfish">私たちについて</a>
-          </div>
-          <div class="footer__nav-column">
-            <a href="<?php echo esc_url(home_url("/information")) ?>" class="footer__text hover-opacity starfish">ダイビング情報</a>
-            <a href="<?php echo esc_url(home_url("/information")) ?>" class="footer__text hover-opacity">ライセンス講習</a>
-            <a href="<?php echo esc_url(home_url("/information")) ?>" class="footer__text hover-opacity">体験ダイビング</a>
-            <a href="<?php echo esc_url(home_url("/information")) ?>" class="footer__text hover-opacity">ファンダイビング</a>
-            <a href="<?php echo esc_url(home_url('/blog')); ?>" class="footer__text hover-opacity starfish">ブログ</a>
-          </div>
-        </div>
-        <div class="footer__wrapper">
-          <div class="footer__nav-column">
-            <a href="<?php echo esc_url(get_post_type_archive_link('voice')); ?>" class="footer__text hover-opacity starfish">お客様の声</a>
-            <a href="<?php echo esc_url(home_url("/price")) ?>" class="footer__text hover-opacity starfish">料金一覧</a>
-            <a href="<?php echo esc_url(home_url("/price")) ?>" class="footer__text hover-opacity">ライセンス講習</a>
-            <a href="<?php echo esc_url(home_url("/price")) ?>" class="footer__text hover-opacity">体験ダイビング</a>
-            <a href="<?php echo esc_url(home_url("/price")) ?>" class="footer__text hover-opacity">ファンダイビング</a>
-          </div>
-          <div class="footer__nav-column">
-            <a href="<?php echo esc_url(home_url("/faq")) ?>" class="footer__text hover-opacity starfish">よくある質問</a>
-            <a href="<?php echo esc_url(home_url("/privacy-policy")) ?>" class="footer__text hover-opacity starfish">プライバシー<br class="u-mobile">ポリシー</a>
-            <a href="<?php echo esc_url(home_url("/terms-of-service")) ?>" class="footer__text hover-opacity starfish">利用規約</a>
-            <a href="<?php echo esc_url(home_url("/contact")) ?>" class="footer__text hover-opacity starfish">お問い合わせ</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <small class="footer__copyright">Copyright &copy; 2021 - <?php echo wp_date("Y");?> CodeUps LLC. All Rights Reserved.</small>
-  </footer>
-  <a href="#" class="page-top" id="js-page-top"><img src="<?php echo esc_url( get_theme_file_uri('/assets/images/to-top.png') ); ?>" alt="ページの一番上へ戻る"></a>
-  <?php wp_footer(); ?>
+  <small class="footer__copyright">Copyright &copy; <?php echo wp_date("Y"); ?> K FIT GYM . All Rights Reserved.</small>
+</footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
