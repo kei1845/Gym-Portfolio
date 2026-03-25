4a352ccf0f89a316c6fa19a9ec56c7feedad6a8a
kei1845 <elsie23418@gmail.com>
2026-03-25 10:41:07 +0900

    404まで

--- gym-portfolio/404.php
+++ gym-portfolio/404.php
@@ -1,14 +1,23 @@
 <?php get_header(); ?>
 
 
-    <main class="sub-error">
-        <?php if (function_exists('bcn_display')) : ?>
-    <div class="inner">
-      <nav class="breadcrumb" vocab="http://schema.org/" typeof="BreadcrumbList">
-        <?php bcn_display(); ?>
-      </nav>
+<section class="sub-mv" id="mv">
+    <div class="sub-mv__img">
+        <img src="<?php echo esc_url(get_theme_file_uri('/assets/images/gym-sub/about-mv.png')); ?>" alt="ジムでランニングマシンを使う女性">
     </div>
-  <?php endif; ?>
+    <div class="sub-mv__content">
+        <h2 class="sub-mv__title">404</h2>
+    </div>
+</section>
+<div class="sub-error-wrapper">
+    <?php if (function_exists('bcn_display')) : ?>
+        <div class="inner">
+            <nav class="breadcrumb" vocab="http://schema.org/" typeof="BreadcrumbList">
+                <?php bcn_display(); ?>
+            </nav>
+        </div>
+    <?php endif; ?>
+    <main class="sub-error">
 
         <div class="error">
             <div class="error-inner inner">
@@ -16,17 +25,13 @@
                 <p class="error-text">申し訳ありません。<br>
                     お探しのページが見つかりません。</p>
 
-
-                <div class="button-wrapper-outer error__button">
-                    <div class="button-wrapper">
-                        <a href="<?php echo esc_url( home_url('/') ); ?>" class="button slide">Page TOP<svg xmlns="http://www.w3.org/2000/svg" width="41"
-                                height="7" viewBox="0 0 41 7" fill="none">
-                                <path d="M0.5 6.5H40.5L33.5 0.5" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg></a>
-                    </div>
+                <div class="button-wrap error__button">
+                    <a href="<?php echo esc_url(home_url('/')); ?>" class="button">Page TOP</a>
                 </div>
+                
             </div>
         </div>
     </main>
+</div>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
